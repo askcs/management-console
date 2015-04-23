cc40aa417e427e56d231b432598480759b705438
helmy <hnurhayati@ask-cs.com>
2015-04-23 16:55:18 +0700

    remove unused controller in main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -55,7 +55,7 @@ require (
     'controllers/domains',
     'controllers/profile',
     'controllers/domain-information',
-    'controllers/tree',
+    'controllers/monitors',
     'directives/appVersion',
     'filters/interpolate',
     'services/version',
